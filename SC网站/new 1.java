ab0754e7b48a82db38220873446a83e94f5e7783
Mandy-108 <59811488+Mandy-108@users.noreply.github.com>
2022-01-19 15:55:48 +0800

    Add files via upload

---
+++ SC网站/new 1.java
@@ -0,0 +1,29 @@
+<div class="search_subject nowrap">
+	<div id="smartSearch" class="smartsearch">
+		
+		<input type="text" class="searchinput" fullsearch="0" 
+		newattachfolder="0" id="subject" name="subject" 
+		autocomplete="off" value="ÓÊ¼þËÑË÷..." style="color: 
+		rgb(160, 160, 160);" onfocus="
+		call('QMFullTextSearch.onfocus')" 
+		onblur="call('QMFullTextSearch.onblur')" aria-label="ËÑË÷">
+		
+		<span class="ss_icon ss_fronticon ss_icon_search" id=
+		"searchIcon"></span>
+		
+		<span class="ss_icon ss_endicon ss_icon_arrowdown" id="
+		subjectsearchLogo" onclick="
+		call('showAdvanceSearchMenu')"></span>
+	</div>
+</div>
+
+
+
+<div class="type-ahead">
+	<input type="text" class="type-ahead__input" placeholder="ÊäÈë¹Ø¼ü´Ê...">
+	<svg class="icon" width="24" height="24" 
+	xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" 
+	clip-rule="evenodd">
+		<path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z"></path></svg>
+	<ul class="type-ahead__suggestions hidden"></ul>
+</div>
